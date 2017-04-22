fbac34302ddae3c19e242274b76288a1f736fcba
ozh <ozh@ozh.org>
2017-04-22 10:13:36 +0200

    Fix (!) tests for PHP 7+

--- tests/utils/bootstrap.php
+++ tests/utils/bootstrap.php
@@ -33,5 +33,13 @@ define( 'BM_TESTJS', BM_ROOTDIR . '/tests/utils/PHP_Phantom/test.js' );
 // Path to data dir containing JS snippets
 define( 'BM_DATA_DIR', BM_ROOTDIR . '/tests/data' );
 
-// Past this point, tests will start
+// PHPUnit 6 compatibility for previous versions
+if ( class_exists( 'PHPUnit\Runner\Version' ) && version_compare( PHPUnit\Runner\Version::id(), '6.0', '>=' ) ) {
+    class_alias( 'PHPUnit\Framework\Assert',        'PHPUnit_Framework_Assert' );
+    class_alias( 'PHPUnit\Framework\TestCase',      'PHPUnit_Framework_TestCase' );
+    class_alias( 'PHPUnit\Framework\Error\Error',   'PHPUnit_Framework_Error' );
+    class_alias( 'PHPUnit\Framework\Error\Notice',  'PHPUnit_Framework_Error_Notice' );
+    class_alias( 'PHPUnit\Framework\Error\Warning', 'PHPUnit_Framework_Error_Warning' );
+}
 
+// Past this point, tests will start
